0147e3d245f389df546da47792f240e3522ea1da
Ali Madooei <alimadooei@gmail.com>
2021-10-25 21:06:09 -0400

    Add header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <Container className="p-3">
+      <Header />
+    </Container>
+  );
 }
 
 export default App;
